d38107036ca6661432d5a92de47f6cb69aabb0df
Jair Cueva Junior <junior@dinhost.com.br>
2024-02-14 15:26:57 -0300

    fix typo

--- scripts/create-replace-list.php
+++ scripts/create-replace-list.php
@@ -9,5 +9,5 @@ $host1 = $rds_old['host'];
 $host2 = $rds_new['host'];
 
 eco('Search for files with the old HOSTNAME');
-$cmd = "grep --include '*.php' --include '.env' {$host1}' /var/www -RIFl > replace-list.txt";
+$cmd = "grep --include '*.php' --include '.env' '{$host1}' /var/www -RIFl > replace-list.txt";
 exec_cmd($cmd);
